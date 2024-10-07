80f51850ba9b4b47780e3705cd4ed5ebae4c0e71
Erickb <wr.t8r1@gmai.com>
2024-10-07 14:25:58 -0700

    Adding tabs nav bar

--- popup.js
+++ popup.js
@@ -63,3 +63,27 @@ document
       }
     );
   });
+
+const homeIcon = document.querySelector('.home-icon');
+const groupIcon = document.querySelector('.group-icon');
+const settingsIcon = document.querySelector('.settings-icon');
+
+const homeSection = document.querySelector('.lazy-section');
+const groupSection = document.querySelector('.group-section');
+const settingsSection = document.querySelector('.settings-section');
+
+groupSection.style.display = 'none';
+settingsSection.style.display = 'none';
+
+// Function to switch between sections
+function switchSection(sectionToShow) {
+  homeSection.style.display = 'none';
+  groupSection.style.display = 'none';
+  settingsSection.style.display = 'none';
+
+  sectionToShow.style.display = 'block';
+}
+
+homeIcon.addEventListener('click', () => switchSection(homeSection));
+groupIcon.addEventListener('click', () => switchSection(groupSection));
+settingsIcon.addEventListener('click', () => switchSection(settingsSection));
